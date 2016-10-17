19958863c471a0b4e3de1264ec847ca6ddf6e6e8
Rowayda Khayri <rowayda_khayri@yahoo.com>
2016-10-18 01:17:13 +0200

    change in month_report.php

--- month_report.php
+++ month_report.php
@@ -43,7 +43,7 @@
                             . "time(a.datetime) as time "
                             . "from attendance a "
                             . "left join employee e on a.employee_id = e.id "
-                            . "where a.process = 2 and a.deleted_at IS NULL ";
+                            . "where a.process = 2  ";
                     
                     
 
