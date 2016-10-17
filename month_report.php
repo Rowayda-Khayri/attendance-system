e99bc4452e76312c93da0462c18b92e3c1e50c6b
Rowayda Khayri <rowayda_khayri@yahoo.com>
2016-10-17 19:09:50 +0200

    add check-in & check-out time in 3rd & 4th column

--- month_report.php
+++ month_report.php
@@ -21,20 +21,32 @@
                     $dbc=  mysqli_connect('localhost', 'root' , 'iti36', 'attendance_system_db')
                     or die('Error in db connection');
 
-                    $query = "select *, e.name as employee_name, date(a.datetime) as date "
+                    $query = "select *, e.name as employee_name, date(a.datetime) as date, "
+                            . "time(a.datetime) as time "
                             . "from attendance a "
-                            . "left join employee e on a.employee_id = e.id ;";
+                            . "left join employee e on a.employee_id = e.id "
+                            . "where process = 1 ;";
+                    
+                    $query2 = "select *, e.name as employee_name, date(a.datetime) as date, "
+                            . "time(a.datetime) as time "
+                            . "from attendance a "
+                            . "left join employee e on a.employee_id = e.id "
+                            . "where process = 2 ;";
 
                     $result = mysqli_query($dbc, $query);
+                    
+                    $result2 = mysqli_query($dbc, $query2);
 
                     for($i=0;$i<=$employees;$i++) {
                          $employees=mysqli_fetch_array($result);
+                         $employees2=mysqli_fetch_array($result2);
+                         
                     ?>
                         <tr>
                             <td><?php echo $employees['employee_name'] ;?></td>
                             <td><?php echo $employees['date'] ;?></td>
-                            <td><?php echo $employees['employee_name'] ;?></td>
-                            <td><?php echo $employees['employee_name'] ;?></td>
+                            <td><?php echo $employees['time'] ;?></td>
+                            <td><?php echo $employees2['time'] ;?></td>
                         </tr>
                             <?php }?>
                         
