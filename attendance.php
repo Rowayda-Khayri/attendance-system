7fa2b5e38efd6acb5ae22edaffd2c99f10fd8508
Rowayda Khayri <rowayda_khayri@yahoo.com>
2016-10-17 22:22:13 +0200

    update checkout for the same day

--- attendance.php
+++ attendance.php
@@ -21,10 +21,13 @@
             ?>
             <div> <?php echo " Hello $name, you are checked-in "; ?></div> <br/>
             <?php
+            
             ////DB connection 
+            
             $dbc=  mysqli_connect('localhost', 'root' , 'iti36', 'attendance_system_db')
                     or die('Error in db connection');
 
+            // select last check-in for choosed employee
             $query4 = "select a.datetime "
                     . "from attendance a "
                     . "left join employee e on a.employee_id = e.id "
@@ -44,7 +47,7 @@
             
             <?php
             
-            if ( $last_check_in_day!=$today){
+            if ( $last_check_in_day!=$today){ // insert check-in only if not checked-in today
                 $query = "insert into attendance (process,datetime,employee_id) "
                         . "values(1,NOW(),(select id from employee where name='$name'));";
 
@@ -69,10 +72,44 @@
             <div> <?php echo " Hello $name, you are checked-out "; ?></div> <br/>
             <?php
             ////DB connection 
-        
+            
             $dbc=  mysqli_connect('localhost', 'root' , 'iti36', 'attendance_system_db')
                     or die('Error in db connection');
 
+            // select last check-out for choosed employee
+            $query4 = "select a.datetime "
+                    . "from attendance a "
+                    . "left join employee e on a.employee_id = e.id "
+                    . " where e.name='$name' and a.process= 2 limit 1;";
+
+            $result4 = mysqli_query($dbc, $query4) 
+                    or die("Error querying DB ");
+            
+            $row= mysqli_fetch_array($result4);
+            $last_check_out = $row[0];
+            $last_check_out_day = date("d/m/Y", strtotime($last_check_out));
+            $today=date("d/m/Y");
+            ?>
+            <div> <?php echo $last_check_out_day; ?></div> <br/>
+            <div> <?php echo  date("d/m/Y"); ?> </div> <br/>
+                
+            <?php
+            //delete last check-out  
+            
+            if ( $last_check_out_day=$today){ 
+                $query2 = "update attendance "
+                        . "set deleted_at = NOW() "
+                        . "where employee_id= (select id from employee where name = '$name') "
+                        . "and process= 2";
+
+                $result2 = mysqli_query($dbc, $query2) 
+                        or die("Error querying DB ");
+                ?>
+                <div> <?php echo  "your check-out has been updated" ;?> </div> <br/>
+
+                <?php  
+            } 
+            
             $query = "insert into attendance (process,datetime,employee_id) "
                     . "values(2,NOW(),(select id from employee where name='$name'));";
 
